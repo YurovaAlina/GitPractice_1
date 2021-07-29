be1146f174f7809661e5b614f99885d97b0a9ae6
YurovaAlina <kulunbiegova@gmail.com>
2021-07-29 13:45:13 -0500

    Did it again

--- src/day3/SetPractice.java
+++ src/day3/SetPractice.java
@@ -3,5 +3,6 @@ package day3;
 public class SetPractice {
     public static void main(String[] args) {
         System.out.println("I need to type smth again");
+        System.out.println("Again");
     }
 }
