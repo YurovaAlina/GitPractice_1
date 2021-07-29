01517dfdb9f5273ef3765e9430fb8fa247ce0829
YurovaAlina <kulunbiegova@gmail.com>
2021-07-29 13:34:12 -0500

    added smth else

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -3,5 +3,6 @@ package day3;
 public class ListPractice {
     public static void main(String[] args) {
         System.out.println("We study Collection");
+        System.out.println("It is hard to understand");
     }
 }
