82c047c0ab52d7a7db2049fa8914179180cd8bcc
YurovaAlina <kulunbiegova@gmail.com>
2021-07-29 13:45:39 -0500

    And again

--- src/day3/SetPractice.java
+++ src/day3/SetPractice.java
@@ -4,5 +4,6 @@ public class SetPractice {
     public static void main(String[] args) {
         System.out.println("I need to type smth again");
         System.out.println("Again");
+        System.out.println("One more time");
     }
 }
