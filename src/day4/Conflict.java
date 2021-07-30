c34b6acd2432a6e9f2efba66dc56457c2425eef9
YurovaAlina <kulunbiegova@gmail.com>
2021-07-30 09:48:00 -0500

    collection2 edit is done

--- src/day4/Conflict.java
+++ src/day4/Conflict.java
@@ -1,12 +1,13 @@
 package day4;
 
 public class Conflict {
-<<<<<<< HEAD
+
     //this is master branch content
     //This is my final version
-=======
+
     //This is collection branch content
         //I hope merge will happen
+    //More work needs to be done here
 
->>>>>>> collection
+
 }
