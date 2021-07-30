f8d4ed5ccc4908a5363df8ec3d5194a22a071984
YurovaAlina <kulunbiegova@gmail.com>
2021-07-30 09:50:00 -0500

    master collection work is done

--- src/day4/Conflict.java
+++ src/day4/Conflict.java
@@ -1,12 +1,14 @@
 package day4;
 
 public class Conflict {
-<<<<<<< HEAD
+
     //this is master branch content
     //This is my final version
-=======
+
     //This is collection branch content
         //I hope merge will happen
+    //This is the change collection2 branch doesn't know about
+    
 
->>>>>>> collection
+
 }
